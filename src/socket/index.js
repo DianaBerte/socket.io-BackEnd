2e3c8c124011d039c2aa4aad318241e2a745f49b
DianaBerte <dianaberte.go@gmail.com>
2023-04-13 16:53:36 +0200

    submit username working

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,5 +2,21 @@ let onlineUsers = []
 
 export const newConnectionHandler = socket => {
     console.log("New client just connected. Client's id is: ", socket.id)
+
+    //1 emitting welcome message to newly connected client
     socket.emit("welcome", { message: `Welcome, ${socket.id}` })
+
+    //2 listening to event "setUsername" called by FE
+    socket.on("setUsername", payload => {
+        console.log(payload)
+
+        //2.1 after receiving username, we keep track of it together with socket.id:
+        onlineUsers.push({ username: payload.username, socketId: socket.id })
+
+        //2.2 sending the list of online users to the newly connected user:
+        socket.emit("loggedIn", onlineUsers)
+
+        //2.3 informing everyone else about the newly connected user:
+        socket.broadcast.emit("updateOnlineUsersList", onlineUsers)
+    })
 }
